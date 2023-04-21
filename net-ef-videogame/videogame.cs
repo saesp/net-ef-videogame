9f658c366f1bd145c4feb950e57c4e74c1f9a6cf
Salvatore Esposito <esposito.00@outlook.it>
2023-04-21 15:25:11 +0200

    Add project files.

---
+++ net-ef-videogame/videogame.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+//Devono essere presenti tutte le funzionalità dell’esercizio originale.
+//Aggiungiamo anche un’altra voce al menu :
+//inserisci una nuova software house
+//Fatto questo, ogni volta che creiamo un nuovo videogioco dobbiamo abbinargli la software house che l’ha prodotto (che dobbiamo aver precedentemente inserito in tabella), chiedendo all’utente l’id della software house e impostandolo nell’entity del videogame.
+//Realizzare quindi tutte le entity e le migration necessarie per creare il database e implementare tutte le richieste dell’esercizio.
+//BONUS : aggiungere un’altra voce di menu
+//stampa tutti i videogiochi prodotti da una software house (all’utente verrà chiesto l’id della software house della quale mostrare i videogame)
+//NB: per effettuare la ricerca sui videogames contenenti una stringa usare la sintassi:
+//.Where(m => m.Name.Contains(stringaDaRicercare))
+namespace net_ef_videogame
+{
+    //[Table("videogames")]  //per cambiare il nome
+    public class Videogame
+    {
+        [Key] public string VideogameId { get; set; }
+        [Required] public string Name { get; set; }
+        public string? Overview { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        
+        public int SoftwareHouseId { get; set; }
+        //public SoftwareHouse SoftwareHouse { get; set; }
+
+    }
+}
