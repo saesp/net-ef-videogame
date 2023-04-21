4e4a3a986c0122b7eac48eaaccd6c03abaa8ff6a
Salvatore Esposito <esposito.00@outlook.it>
2023-04-21 15:30:23 +0200

    added SoftwareHouse class

--- net-ef-videogame/softwareHouse.cs
+++ net-ef-videogame/softwareHouse.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace net_ef_videogame
 {
-    internal class softwareHouse
+    public class SoftwareHouse
     {
+        [Key] public string SoftwareHouseId { get; set; }
+        [Required] public string Name { get; set; }
+        public string Address { get; set; }
+
+        //one (SoftwareHouse) to many (Videogames)
+        public List<Videogame> Videogames { get; set; }
     }
 }
