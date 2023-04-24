69aa9127b1abc84c8a108aa4bef87defd1da993a
Salvatore Esposito <esposito.00@outlook.it>
2023-04-24 16:29:59 +0200

    changes

--- net-ef-videogame/Program.cs
+++ net-ef-videogame/Program.cs
@@ -3,10 +3,6 @@ using net_ef_videogame;
 
 namespace net_ef_videogame
 {
-    // 2 ricercare un videogioco per id
-    // 3 ricercare tutti i videogiochi aventi il nome contenente una determinata stringa inserita in input
-    // 4 cancellare un videogioco
-    // 5 chiudere il programma
     internal class Program
     {
         static void Main(string[] args)
@@ -17,7 +13,7 @@ namespace net_ef_videogame
 
             Console.WriteLine("1 | Inserisci un nuovo videogioco");
             Console.WriteLine("2 | Inserisci una nuova software house");
-            Console.WriteLine("3 | Ricerca un videogioco per nome");
+            Console.WriteLine("3 | Ricerca uno o più videogiochi per nome");
             Console.WriteLine("4 | Ricerca un videogioco per id");
             Console.WriteLine("5 | Stampa la lista dei videogiochi");
             Console.WriteLine("6 | Cancella un videogioco \n");
